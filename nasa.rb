c8763c05eae6450ef47ff7eb3c6c5b0dbc24d5b6
Fabian Conejo <fabianconejosegu@gmail.com>
2021-02-08 13:14:01 -0600

    Rendering results on HTML file then applying styles with CSS

--- nasa.rb
+++ nasa.rb
@@ -11,20 +11,42 @@ def request(url, token = nil)
     return JSON.parse(response.read_body)
 end
 
-def build_web_page(info_hash)
-    File.open("rover_curiosity.html", "w") do |hash|
-        hash.puts "<html>" 
-        hash.puts "<head>"
-        hash.puts "</head>"
-        hash.puts "<body>"
-        hash.puts "<ul>"
-        hash.puts "<li><img></li>"
-        hash.puts "<li><img></li>"
-        hash.puts "</ul>"
-        hash.puts "</body>"
-        hash.puts "</html>"
-    end   
-end    
+def build_web_page(nasa_hash)#=> here goes a hash returned from the request method
+    File.open("nasa.html", "w") do |key|
+        nasa_hash.each do |value| #=> class array
 
-nasa_hash = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1", "pofO238yhgKQk0AxaSjYiBHCOQYuV51ax7AfSmwT")
-build_web_page(nasa_hash)
+            photos = value[1]
+
+            
+            key.puts "<html>" 
+            key.puts "<head>"
+            key.puts "<title>NASA | Curiosity</title>"
+            key.puts "<link rel='stylesheet' href='style.css'>"
+            key.puts "</head>"
+            key.puts "<body>"
+            key.puts "<header>"
+            key.puts "<img src='nasa.png'>"
+            key.puts "<h1>Mars Rover Photos</h1>"
+            key.puts "</header>"
+            key.puts "<ul>"
+            key.puts "<div class='img-wrapper'>"
+
+            photos.each do |photo|
+                key.puts "<h2>Date: #{photo["earth_date"]}</h2>"
+                key.puts "<li><img src='#{photo["img_src"]}' alt='id: #{photo["id"]}'></li>"
+            end    
+
+            key.puts "</div>"
+            key.puts "</ul>"
+            key.puts "</body>"
+            key.puts "</html>"
+        end    
+    end   
+end
+ 
+#def  photos_count()
+
+#end
+
+nasa_request = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1", "pofO238yhgKQk0AxaSjYiBHCOQYuV51ax7AfSmwT")
+build_web_page(nasa_request)
